9aa19846225121779d1c09889ff502c776ec1623
TaohRihze <github@rihze.dk>
2023-09-15 17:22:46 +0200

    Add project files.

---
+++ SongSuggestCore/DataHandling/LastRankedSuggestions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SongSuggestNS
+{
+    public class LastRankedSuggestions
+    {
+        public bool isLibraryLoaded { get; set; } = false;
+        public SongSuggest songSuggest { get; set; }
+        public List<String> lastSuggestions { get; set; }
+
+        public void Load()
+        {
+            lastSuggestions = songSuggest.fileHandler.LoadRankedSuggestions();
+        }
+
+        public void Save()
+        {
+            songSuggest.fileHandler.SaveRankedSuggestions(lastSuggestions);
+        }
+
+        public String GetRank(String hash, String difficulty)
+        {
+            String id = songSuggest.songLibrary.Contains(hash, difficulty) ? songSuggest.songLibrary.GetID(hash, difficulty) : "";
+            return lastSuggestions.Contains(id)?""+(lastSuggestions.IndexOf(id)+1):"";
+        }
+
+        public String GetRankCount()
+        {
+            return "" + lastSuggestions.Count;
+        }
+    }
+}
